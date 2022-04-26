009736938a6f4d18c58c7446c3311d9bc490597a
Ronald <ronald@pop-os.localdomain>
2022-04-26 13:49:40 -0300

    push

--- cypress/integration/api-spy.spec.js
+++ cypress/integration/api-spy.spec.js
@@ -1,26 +1,8 @@
 describe('Spiando una API', () => {
-    beforeEach(() => {
-        cy.visit(Cypress.config().baseUrl_test)
-    });
-
+  
     it('Add new todo by UI', function(){
-        cy.intercept('POST', '/todos').as('newTodo')
-
-        cy.get('input#add-todo').type('new-todo{enter}')
-
-        cy.wait('@newTodo').its('response.statusCode').should('eq', 201)
-
-        cy.get('@newTodo').should(request => {
-            cy.log(request)
-            expect(request.response.body.title).to.eql('new-todo')
-
-            const newTodoID = request.response.body.id
-
-            cy.wrap(newTodoID).as('newTodoID')
-        })
-    })
-
-    after(function(){
-        cy.request('DELETE', `/todos/${this.newTodoID}`)
+        cy.visit('http://localhost:8080/todo')
+        
+        cy.get('.todo-list li').should('have.length', 2)
     })
 });
